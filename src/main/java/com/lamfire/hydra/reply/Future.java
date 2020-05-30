3414f421331b625e51e57fbe66a8177c3a2a02bb
text <text@1024.com>
2020-05-30 11:26:08 +0800

    add async method

--- src/main/java/com/lamfire/hydra/reply/Future.java
+++ src/main/java/com/lamfire/hydra/reply/Future.java
@@ -8,24 +8,24 @@ public class Future {
     private final long createAt = System.currentTimeMillis();
     private Message response;
     private long timeout = 30000;
+    private boolean responseReceived = false;
+    private OnReplyResponseListener onReplyResponseListener;
 
     void setTimeout(long timeoutMillis){
         this.timeout = timeoutMillis;
     }
 
     public synchronized Message getResponseMessage() throws TimeoutException {
-        if(response == null){
+        if(response == null && !responseReceived){
             try {
                 this.wait(timeout);
             } catch (InterruptedException e) {
 
             }
+            if(response == null){
+                throw new TimeoutException();
+            }
         }
-
-        if(response == null){
-            throw new TimeoutException();
-        }
-
         return response;
     }
 
@@ -35,11 +35,18 @@ public class Future {
 
     synchronized void onResponse(Message response){
         this.response = response;
+        this.responseReceived = true;
         this.notifyAll();
+        if(onReplyResponseListener != null){
+            onReplyResponseListener.onReplyResponse(response);
+        }
     }
 
+    public void setOnReplyResponseListener(OnReplyResponseListener onReplyResponseListener) {
+        this.onReplyResponseListener = onReplyResponseListener;
+    }
 
     boolean isTimeout(){
-        return System.currentTimeMillis() - createAt - timeout > 0;
+        return responseReceived?false:System.currentTimeMillis() - createAt - timeout > 0;
     }
 }
---
+++ src/main/java/com/lamfire/hydra/reply/OnReplyResponseListener.java
@@ -0,0 +1,7 @@
+package com.lamfire.hydra.reply;
+
+import com.lamfire.hydra.Message;
+
+public interface OnReplyResponseListener {
+    void onReplyResponse(Message response);
+}
